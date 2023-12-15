97dfa2c334c2ce19b639237e0590c886ed845db9
ImPrathmesh07 <153943216+ImPrathmesh07@users.noreply.github.com>
2023-12-16 00:12:04 +0530

    functions.dart

--- functions.dart
+++ functions.dart
@@ -1,6 +1,6 @@
 // Here we are creating a function
 
-void add({num1, num2}) {
+void add(num1, num2) {
   num1 + num2;
 
 
@@ -11,5 +11,23 @@ void demo(){
 }
 //  Or write in this way to call the function
 void demo(){
-  add (num2: 5,num1: 3);  // We do this beacuse in upside way values assign on how function is wriition means num1=5 and num2=3
+  add (5, 3);
+
+
+//Positional: The position of an argument determines which parameter receives the value
+
+void add(a, b) { // a & b are positional parameters
+  print(a + b); // print() is a built-in function that will be explained later
 }
+ 
+add(5, 10); // 5 is used as a value for a, because it's the first argument; 10 is used as a value for b
+
+  
+//Named: The name of an argument determines which parameter receives the value
+
+void add({a, b}) { // a & b are named parameters (because of the curly braces)
+  print(a + b); 
+}  
+ 
+add(b: 5, a: 10); // 5 is used as a value for b, because it's assigned to that name; 10 is used as a value for a
+
