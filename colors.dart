77e39e66456b0e1b83ac78685f26dfb74f7e3bde
ImPrathmesh07 <153943216+ImPrathmesh07@users.noreply.github.com>
2023-12-16 01:42:05 +0530

    Create colors.dart

---
+++ colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 154, 103, 244),  // ctrl+space give so many feturte which we want to add
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
+}
