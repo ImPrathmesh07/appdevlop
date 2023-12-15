7861191e04cda8ab7b03a845ab5140426ba341e8
ImPrathmesh07 <153943216+ImPrathmesh07@users.noreply.github.com>
2023-12-16 00:04:53 +0530

    functions.dart

--- functions.dart
+++ functions.dart
@@ -9,3 +9,7 @@ void add({num1, num2}) {
 void demo(){
   add (5,3);
 }
+//  Or write in this way to call the function
+void demo(){
+  add (num2: 5,num1: 3);  // We do this beacuse in upside way values assign on how function is wriition means num1=5 and num2=3
+}
