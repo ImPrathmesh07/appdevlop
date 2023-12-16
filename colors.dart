9481eedb976f4c348774632eec564435940194cd
ImPrathmesh07 <153943216+ImPrathmesh07@users.noreply.github.com>
2023-12-16 12:37:39 +0530

    colors.dart

--- colors.dart
+++ colors.dart
@@ -12,3 +12,54 @@ void main() {
     ),
   );
 }
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(230, 255, 255, 255),
+              Color.fromARGB(255, 255, 255, 255)]),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 75, 98)
+              ], 
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+              ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
